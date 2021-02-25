5bda6a5e7581c8235459ae0dde930842a8ada09f
焦俊峰 <jiaojunfeng@mrhi.com>
2021-02-25 16:06:50 +0800

    更改  重新上传

--- src/main/java/com/haier/hailicommontlib/mvp/model/baidulocation/LocationService.java
+++ src/main/java/com/haier/hailicommontlib/mvp/model/baidulocation/LocationService.java
@@ -17,6 +17,7 @@ import com.haier.hailicommontlib.mvp.model.utils.LogUtil;
  */
 public class LocationService {
 
+
     private static LocationClient client = null;
     private static LocationClientOption mOption;
     private String city = "";//城市
